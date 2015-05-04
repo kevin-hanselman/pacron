f835b926469c3c00d9723b4c78f1a7b0f83ebdd4
Kevin Hanselman <kevin.hanselman@gmail.com>
2015-05-04 11:00:30 -0400

    in pacman.log, ignore installing pacman deps for AUR pkgs

--- config/pacron/hooks/last-pacman-log.sh
+++ config/pacron/hooks/last-pacman-log.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-line=$(grep -n "Running 'pacman" /var/log/pacman.log | grep -v '\-U' | tail -n 1 | cut -d: -f1)
-
+line=$(grep -n "Running 'pacman" /var/log/pacman.log | grep -vE '\-U|\-\-noconfirm \-\-asdeps' | tail -n 1 | cut -d: -f1)
 sed "1,$(($line - 1))d" /var/log/pacman.log
 
